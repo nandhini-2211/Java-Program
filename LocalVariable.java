1cea2049f905cbf491ad54ac37ee536365a18bc6
nandhini-2211 <136336009+nandhini-2211@users.noreply.github.com>
2024-12-19 23:19:45 +0530

    Add files via upload

---
+++ LocalVariable.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class LocalVariable {
+	 public void Addition(){
+	        int a=20;
+	        int b=30;
+	        int sum=a+b;
+	        System.out.println(sum);
+	        
+	    }
+	    public static void main(String[] args) {
+	        LocalVariable obj=new LocalVariable();
+	        obj.Addition();
+	    }
+}
