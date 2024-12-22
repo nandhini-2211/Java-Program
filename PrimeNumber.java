e5c40a995f7fce0f173955e4e5372268d0e0ccd0
nandhini-2211 <136336009+nandhini-2211@users.noreply.github.com>
2024-12-23 00:47:40 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,23 @@
+package Day4;
+import java.util.Scanner;
+public class PrimeNumber {
+	 public static void main(String[] args) {
+	        Scanner obj=new Scanner(System.in);
+	        System.out.println("Enter a Number");
+	        int num=obj.nextInt();
+	        
+	        if(num>1){
+	            boolean isPrime=true;
+	            for(int i=2; i<=Math.sqrt(num);i++){
+	                if(num%i==0){
+	                    isPrime=false;
+	                    break;
+	                }
+	            }
+	            if(isPrime){
+	                System.out.println(num+" is a Prime Number");
+	            }
+	        }
+	    }
+
+}
