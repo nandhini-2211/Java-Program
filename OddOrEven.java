a104e16f82ba79f7cc30107303f07b58d14938cc
nandhini-2211 <136336009+nandhini-2211@users.noreply.github.com>
2024-12-23 00:48:04 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,18 @@
+package Day4;
+import java.util.Scanner;
+public class OddOrEven {
+	 public static void main(String[] args) {
+	        Scanner obj=new Scanner(System.in);
+	        System.out.println("Enter a number:");
+	        int num=obj.nextInt();
+	        if(num%2==0){
+	            System.out.println("It is a Even Number");
+	            
+	        }
+	        else{
+	            System.out.println("It is a Odd Number");
+	        }
+	    }
+	 
+
+}
