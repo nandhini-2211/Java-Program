fc5116146e98cc36544687359d024f8ac254508d
nandhini-2211 <136336009+nandhini-2211@users.noreply.github.com>
2024-12-24 09:01:51 +0530

    Add files via upload

---
+++ MultiplicationTables.java
@@ -0,0 +1,17 @@
+package Day5;
+
+public class MultiplicationTables {
+	 public static void main(String[] args) {
+	        Scanner obj=new Scanner(System.in);
+	        System.out.println("Multiplication Table");
+	        System.out.println("Enter a number:");
+	        int n=obj.nextInt();
+	        int i=1;
+	        while(i<=10){
+	            System.out.println(n +"*" +i+ "="+ n*i);
+	            i++;
+	        }
+	       
+	    }
+
+}
