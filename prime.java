a3213beb84ea69db391143588a02cf880cbf0c5e
nandhini-2211 <136336009+nandhini-2211@users.noreply.github.com>
2024-12-24 09:12:07 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,34 @@
+package Day5;
+
+public class prime {
+	 public static void main(String[] args) {
+	        Scanner obj = new Scanner(System.in);
+	        System.out.println("Enter a Number: ");
+	        int n = obj.nextInt();
+	        System.out.println("The Prime Numbers are ");
+	        
+	        
+	        int num = 2; 
+	        
+	        while (num <= n) {
+	            boolean isPrime = true;
+	            int i = 2;
+	            
+	            while (i <= num / 2) {
+	                if (num % i == 0) {
+	                    isPrime = false;
+	                    break;
+	                }
+	                i++;
+	            }
+	            
+	            if (isPrime) {
+	                System.out.println(num);
+	            }
+	            
+	            num++;
+	        }
+	        
+	        
+	    }
+}
